cba1f6249d67d26e4e1ecabbfbae3e2eca4e67c8
RobertoRamirez0 <ramirez.robertomu22@gmail.com>
2019-08-13 20:44:38 -0400

    added event listeners and used stop propagation function for nested events

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 // Selectors
-let bodyAll = document.querySelector("body");
+let bodyAll = document.querySelector(".body-all");
 let mainNav = document.querySelector(".nav-container");
 let logo = document.querySelector(".logo-heading");
 let navItems = document.querySelectorAll(".nav-link");
@@ -7,15 +7,36 @@ let image = document.querySelectorAll("img");
 let button = document.querySelectorAll(".btn");
 let header = document.querySelectorAll(".intro-one");
 let contentOne = document.querySelectorAll(".content-one");
+let sectionOne = document.querySelector(".first-content");
+let destinationSection = document.querySelectorAll(".destination");
+let destinationBtn = document.querySelectorAll(".btn");
 
 // Init Animations
-TweenMax.from(logo, 0.5, {opacity: 1, y: 200});
+TweenMax.from(logo, 0.5, {opacity: 0, y: 200});
 TweenMax.staggerFrom(navItems, 0.5, {opacity:0, y:200, delay:0.5}, 0.2);
 TweenMax.staggerFrom(header, 0.5, {opacity:0, y:200, delay: 0.5}, 0.2);
 TweenMax.staggerFrom(contentOne, 0.5, {opacity:0, y:200, delay: 0.8}, 0.2);
 TweenMax.from(".image-two", 0.8, {opacity:0, y:200, delay: 1});
-
+// Event Setters
+function zoomImg(event) {
+  event.currentTarget.style.maxWidth = '110%';
+}
+function buttonBg(event) {
+  event.stopPropagation();
+  event.currentTarget.style.backgroundColor = 'black';
+}
+function destinationBg(event) {
+  event.currentTarget.style.backgroundColor = 'blue';
+}
 //Events
-bodyAll.addEventListener('mouseover', () =>{
-  mainNav.style.backgroundColor = 'white';
-})
+image[1].addEventListener('mouseover', zoomImg);
+image[2].addEventListener('mouseover', zoomImg);
+sectionOne.addEventListener('dblclick', (event) => {
+  event.currentTarget.style.backgroundColor = 'green'
+});
+destinationBtn[0].addEventListener('click', buttonBg);
+destinationBtn[1].addEventListener('click', buttonBg);
+destinationBtn[2].addEventListener('click', buttonBg);
+destinationSection[0].addEventListener('click', destinationBg);
+destinationSection[1].addEventListener('click', destinationBg);
+destinationSection[2].addEventListener('click', destinationBg);
